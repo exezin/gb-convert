bf2da545f2cc33daee425029172ca7c249cf35db
siObyte <indieretrostudios@gmail.com>
2016-02-03 00:54:02 +0000

    added png to map conversion support

--- README.md
+++ README.md
@@ -6,27 +6,36 @@ Gameboy-convert is a simple tool to convert png images, into a format supported 
 It currently takes in a Nx8 size png file, and outputs it in an assembly format you can use when developing for the
 Gameboy.  Eventually this will be used in a Gameboy homebrew tutorial I am writing on my website (https://siobyte.xyz).
 
+The tiles conversion takes a 8 pixel high, and X pixels long image and outputs the assembly equivalent.
+
+The map conversion takes in a 256x256 image (32x32 tiles), and matches the tiles in the image, with the tiles from
+your tilesheet, and outputs the assembly equivalent of your map for use on the Gameboy.
 
 ### Make
 ```
-    mkdir build
-    mkdir obj
-    make
+mkdir build
+mkdir obj
+make
 ```
 
 ### Example usage
 ```
-(mytiles.png = 32x8)
+(mytiles.png = 32x8 )
+(mymap.png   = 32x32)
 
-./gb-convert -tiles mytiles.png >> output.txt
+./gb-convert -tiles mytiles.png -map mymap.png >> output.txt
 
     >> output.txt - 4 tiles - 16 bytes each
+    TILE_DATA:
     DB $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
     DB $ff,$ff,$81,$ff,$bd,$c3,$a5,$c3,$a5,$c3,$bd,$c3,$81,$ff,$ff,$ff
     DB $00,$00,$44,$44,$00,$00,$00,$00,$44,$44,$38,$38,$00,$00,$00,$00
     DB $00,$00,$22,$22,$00,$00,$00,$00,$1c,$1c,$22,$22,$00,$00,$00,$00
+	
+    MAP_DATA:
+    DB $01,$01,$00...etc
 
-    Colors should be the following beforehand:
+    Colors should be the following (or close to) beforehand:
     White - (255, 255, 255)
     Grey  - (150, 150, 150)
     DGrey - (80,  80,  80 )
@@ -40,4 +49,3 @@ Gameboy.  Eventually this will be used in a Gameboy homebrew tutorial I am writi
 
 ### Todo
 - Better Support for arbitrary image sizes
-- Ability to convert PNG files to map format
--- main.c
+++ main.c
@@ -4,10 +4,9 @@
 	-siobyte.xyz
 
 	example usage:
-	"./gb-convert -tiles mytiles.png >> output.txt"
+	"./gb-convert -tiles mytiles.png -map mymap.png >> output.txt"
 
 	TODO:
-	-map editing/converting
 	-better image size support
 */
 
@@ -17,11 +16,22 @@
 #include <stdlib.h>
 
 void ConvertTiles(const char *file);
+void ConvertMap(const char *file);
+
+int tilecount = 0;
+int bytecount = 0;
+unsigned short int tiledata[192*16];
+unsigned short int mapdata[32*32];
 
 int main(int argc, char *argv[])
 {
-	if (strcmp("-tiles", argv[1]) == 0)
-		ConvertTiles(argv[2]);
+	int i;
+	for (int i=0; i<argc; i++) {
+		if (strcmp("-tiles", argv[i]) == 0)
+			ConvertTiles(argv[i+1]);
+		else if (strcmp("-map", argv[i]) == 0)
+			ConvertMap(argv[i+1]);
+	}
 
 	return 1;
 }
@@ -30,7 +40,7 @@ void ConvertTiles(const char *file)
 {
 	int n, w, h;
 	unsigned char *data = stbi_load(file, &w, &h, &n, 4);
-	
+
 	if (data == NULL) {
 		printf("Failed to load file %s\n", file);	
 		return;	
@@ -38,39 +48,38 @@ void ConvertTiles(const char *file)
 
 	/* 8x8 pixels, 2 bits per pixel, 2 bytes per line */
 	int i, x, y;
-	int byte = 0;
-	int tilecount = w/8;
-	unsigned short int tiledata[192*16];
+	tilecount = floor(w/8);
 	for (int i=0; i<tilecount; i++) {
 		for (int y=0; y<8; y++) {
 			int shift = 7;
 			for (int x=0; x<8; x++) {
 				int index = 4 * (((y * w) + x) + (i*8));
-				unsigned short int color  = 0b00000001;
-				unsigned short int color2 = 0b00000001;
+				unsigned short int cbit1  = 0b00000001;
+				unsigned short int cbit2  = 0b00000001;
 
 				if (data[index] > 60 && data[index] < 120) {
-					color  = 0b00000000;
-					color2 = 0b00000001;
+					cbit1  = 0b00000000;
+					cbit2  = 0b00000001;
 				} else if (data[index] > 119 && data[index] < 180) {
-					color  = 0b00000001;
-					color2 = 0b00000000;
+					cbit1  = 0b00000001;
+					cbit2  = 0b00000000;
 				} else if (data[index] > 179) {
-					color  = 0b00000000;
-					color2 = 0b00000000;
+					cbit1  = 0b00000000;
+					cbit2  = 0b00000000;
 				}
 				
-				tiledata[byte]   |= (color << shift);
-				tiledata[byte+1] |= (color2 << shift);
+				tiledata[bytecount]   |= (cbit1 << shift);
+				tiledata[bytecount+1] |= (cbit2 << shift);
 				shift--;
 			}
-			byte+=2;
+			bytecount+=2;
 		}
 	}
 
 	/* print out the bytes */
 	int c;
-	for (int i=0; i<byte; i++) {
+	printf("TILE_DATA: \n");
+	for (int i=0; i<bytecount; i++) {
 		if (c == 0)
 			printf("\nDB ");
 
@@ -89,15 +98,106 @@ void ConvertTiles(const char *file)
 		if (c > 15)
 			c = 0;
 	}
+
+	printf("\n\n");
 }
 
+void ConvertMap(const char *file)
+{
+	int n, w, h;
+	unsigned char *data = stbi_load(file, &w, &h, &n, 4);
 
+	if (data == NULL) {
+		printf("Failed to load file %s\n", file);	
+		return;	
+	}
 
+	/* clear map data */
+	int i;
+	for (int i=0; i<32*32; i++)
+		mapdata[i] = 0;
 
+	/* match each tile in the map to a tile in the tiles  */
+	int maptilecount = floor(w/8)*floor(h/8);
+	int byte         = 0;
+	int tile         = 0;
+	int tilex        = 0;
+	int tiley        = 0;
+	int x, y;
 
+	for (int i=0; i<maptilecount; i++) {
+		byte = 0;
+		tile = 0;
 
+		unsigned short int singletile[16];
+		for (int y=0; y<16; y++)
+			singletile[y] = 0;
+	
+		/* get single tile data */
+		for (int y=0; y<8; y++) {
+			int shift = 7;
+			for (int x=0; x<8; x++) {					
+				int index = 4 * (((y+tiley) * w) + (x + tilex));
+				unsigned short int cbit1  = 0b00000001;
+				unsigned short int cbit2  = 0b00000001;
+				
+				if (data[index] > 60 && data[index] < 120) {
+					cbit1  = 0b00000000;
+					cbit2  = 0b00000001;
+				} else if (data[index] > 119 && data[index] < 180) {
+					cbit1  = 0b00000001;
+					cbit2  = 0b00000000;
+				} else if (data[index] > 179) {
+					cbit1  = 0b00000000;
+					cbit2  = 0b00000000;
+				}
+				
+				singletile[byte]   |= (cbit1 << shift);
+				singletile[byte+1] |= (cbit2 << shift);
+				shift--;
+			}
+			byte+=2;
+		}
 
+		tilex += 8;
+		if (tilex > w-1) {
+			tiley += 8;
+			tilex  = 0;	
+		}	
 
+		/* match against on in tilemap */
+		int c;
+		for (int c=0; c<tilecount; c++) {
+			int matchcount = 0;
+			for (int x=0; x<16; x++) {
+				if (singletile[x] == tiledata[x + (c*16)]) {
+					matchcount++;
+				}
+			}
+			if (matchcount > 15) {
+				tile = c;
+				break;
+			}
+		}
+	
+		mapdata[i] = tile;
+	}
 
+	/* print out the map data */
+	printf("MAP_DATA: \n");
+	int c = 0;
+	for (int y=0; y<32; y++) {
+		printf("\nDB ");
+		for (int x=0; x<32; x++) {
+			printf("$");
+			if(mapdata[c] < 0x10)
+				printf("0");
+			printf("%x", mapdata[c]);
+			if (x < 31)
+				printf(",");	
+			c++;
+		}
+	}
+}
 
 
